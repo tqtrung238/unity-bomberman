c427d2b4ad12fec6f3954f544cfd8b69aeae11e6
Quang Trung <tqtrunguet@gmail.com>
2024-04-26 17:24:25 +0700

    bomberman in unity

---
+++ Assets/Scripts/ItemPickup.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class ItemPickup : MonoBehaviour
+{
+    public enum ItemType
+    {
+        ExtraBomb,
+        BlastRadius,
+        SpeedIncrease,
+    }
+
+    public ItemType type;
+
+    private void OnItemPickup(GameObject player)
+    {
+        switch (type)
+        {
+            case ItemType.ExtraBomb:
+                player.GetComponent<BombController>().AddBomb();
+                break;
+            case ItemType.BlastRadius:
+                player.GetComponent<BombController>().explosionRadius++;
+                break;
+            case ItemType.SpeedIncrease:
+                player.GetComponent<MovementController>().speed++;
+                break;
+        }
+
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            OnItemPickup(collision.gameObject);
+        }
+    }
+}
